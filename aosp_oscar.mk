bf2da73b3781863c987ecc3f18175b831b0cf893
Sugakesshaa <sugakesshaa@gmail.com>
2023-09-20 23:09:50 +0800

    oscar: Welcome to PixelOS
    Change-Id: I35a09df46b28595bfd6c79d0c29da031d0e1b792

--- aosp_oscar.mk
+++ aosp_oscar.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oscar/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_oscar
 PRODUCT_DEVICE := oscar
