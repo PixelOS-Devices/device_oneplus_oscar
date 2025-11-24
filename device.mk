844fbcac29f8a51a0e9a0e8aa90c82f6c76edc86
bheatleyyy <sugakesshaa@pixelos.net>
2025-11-24 15:20:35 +0100

    oscar: Nuke keyhandler
    Change-Id: I827f9fb16dbfe598a3e9fc7b1497932848acb8aa

--- device.mk
+++ device.mk
@@ -8,10 +8,6 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Alert slider
-PRODUCT_PACKAGES += \
-    KeyHandler
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
